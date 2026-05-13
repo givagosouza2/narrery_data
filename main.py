86f6cfb5e894fd7d49411aef216cd7b0ddb34ec4
givagosouza2 <147889314+givagosouza2@users.noreply.github.com>
2026-05-13 16:29:49 -0300

    Change condition to check if valor is greater than or equal

--- main.py
+++ main.py
@@ -311,11 +311,11 @@ if arquivo_1 is not None and arquivo_2 is not None:
         st.plotly_chart(fig, use_container_width=True)
 
     for index,valor in enumerate(df_1["tempo"]):
-        if valor == triggerAnkle:
+        if valor >= triggerAnkle:
             df_1["tempo"] = df_1["tempo"] - df_1["tempo"][index]
             break
     for index,valor in enumerate(df_2["tempo"]):
-        if valor == triggerLumbar:
+        if valor >= triggerLumbar:
             df_2["tempo"] = df_2["tempo"] - df_2["tempo"][index]
             break
     
